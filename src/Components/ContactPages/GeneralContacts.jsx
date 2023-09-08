9deb51051fd73150ce9d2f55655edf41cd95e89d
RoboticsDevelopmentName <RoboticsDevelopment@protonmail.com>
2023-09-08 16:51:28 -0400

    Toggle Favorites

--- src/Components/ContactPages/GeneralContacts.jsx
+++ src/Components/ContactPages/GeneralContacts.jsx
@@ -3,16 +3,19 @@ import Contact from "./Contact";
 const GeneralContacts = (props) => {
   return (
     <div
-    className="col-12 py-2"
-    style={{ borderRadius: "10px", backgroundColor: "#323637" }}
+      className="col-12 py-2"
+      style={{ borderRadius: "10px", backgroundColor: "#323637" }}
     >
-
-        <div className="text-center text-white-50">General Contacts</div>
-        <div className="p-2">
-      {props.contacts.map((contact, index) => (
-        <Contact contact={contact} key={index}></Contact>
-      ))}
-    </div>
+      <div className="text-center text-white-50">General Contacts</div>
+      <div className="p-2">
+        {props.contacts.map((contact, index) => (
+          <Contact
+            contact={contact}
+            key={index}
+            favoriteClick={props.favoriteClick}
+          ></Contact>
+        ))}
+      </div>
     </div>
   );
 };
